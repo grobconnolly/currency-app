56150d0da470446a7f6a8eff1a90c50f0d4ae898
Matthew Bell <matthewbell1030@gmail.com>
2020-02-04 00:03:20 -0800

    For logic.js; created global variables, opened document.ready function, and began framing onClick finctions.  I halted on the onclick functions because I have a few questions of functionality that we can discuss on Tuesday 02-04-2020.

---
+++ assets/logic.js
@@ -0,0 +1,45 @@
+// Wait until Dom has completed loading
+$(document).ready(function() {
+
+// Check that script is successfully linked to index.html
+console.log("logic.js is successfully linked");
+
+//-----DECLARING GLOBAL VARIABLES-----
+let titleText = $(".titleText");
+let dateToday = $(".dateToday");
+let pdfButton = $(".pdfButton");
+
+let boxHeader = $(".boxHeader");
+let amountText = $(".amountText");
+
+let box1 = $(".box1");
+let currency1Amount = $(".currency1Amount");
+let updated1 = $(".updated1");
+
+let blankArea = $(".blankArea");
+
+let box2 = $(".box2");
+let currency2Amount = $(".currency2Amount");
+let updated2 = $(".updated2");
+
+let calButton = $(".calButton");
+
+
+//-----BUILDING FUNCTIONS-----
+// Build onClick dropDown function for box 1 and box 2
+$("").on("click", function (event) {
+
+});
+
+
+// Build onClick calculate function for calButton
+$("").on("click", function (event) {
+
+});
+
+
+//-----CALLING FUNCTIONS-----
+
+
+
+});
