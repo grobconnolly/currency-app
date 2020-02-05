e37e33485f66c04617fcb5954d2a191a4ad49f19
Greg Schuman <gcschuman@hotmail.com>
2020-02-04 19:44:51 -0800

    Blank

--- assets/logic.js
+++ assets/logic.js
@@ -47,23 +47,25 @@ $(calButton).on("click", function (event) {
 // Build onClick generate PDF function for 'a'
 // When user clicks 'a', a PDF is generated including 'a screenshot of the app in its current state'.
 // Then a link to a PDF is generated and then opened in the new window.
-$(pdfButton).on("click", function (event) {
-    if (typeof doc.print === 'undefined') {    
-        setTimeout(function(){printDocument(documentId);}, 1000);
-    } else {
-        doc.print();
-    }
-<embed
-    type="application/pdf"
-    src="path_to_pdf_document.pdf"
-    id="pdfDocument"
-    width="100%"
-    height="100%" />
-});
+// $(pdfButton).on("click", function (event) {
+//     if (typeof doc.print === 'undefined') {    
+//         setTimeout(function(){printDocument(documentId);}, 1000);
+//     } else {
+//         doc.print();
+//     }
+// <embed
+//     type="application/pdf"
+//     src="path_to_pdf_document.pdf"
+//     id="pdfDocument"
+//     width="100%"
+//     height="100%" />
+// });
 
 
 //-----CALLING FUNCTIONS-----
-
+button.addEventListener("click", function(name)){
+    fetch("http://data.fixer.io/api/latest?access_key=046374ead830a8a183f4c823ed1d0bc2")
+}
 
 
 });
