07c3f383e029551bdb6bce54f4211b259ffaa153
Matthew Bell <matthewbell1030@gmail.com>
2020-02-04 18:33:31 -0800

    re-pushing my first commit

--- assets/logic.js
+++ assets/logic.js
@@ -12,28 +12,42 @@ let pdfButton = $(".pdfButton");
 let boxHeader = $(".boxHeader");
 let amountText = $(".amountText");
 
+let currency1 = $(".currency1");
+let select = $(".select");
 let box1 = $(".box1");
 let currency1Amount = $(".currency1Amount");
 let updated1 = $(".updated1");
+let updatedTime1 = $("#updatedTime1");
 
 let blankArea = $(".blankArea");
 
+let currency2 = $(".currency2");
 let box2 = $(".box2");
 let currency2Amount = $(".currency2Amount");
 let updated2 = $(".updated2");
+let country2price = $("#country2price");
 
 let calButton = $(".calButton");
 
 
 //-----BUILDING FUNCTIONS-----
-// Build onClick dropDown function for box 1 and box 2
-$("").on("click", function (event) {
+// Build onClick dropDown functions for box 1 and box 2
+// When user clicks dropDown 
+$(select).on("click", function (event) {
 
 });
 
 
 // Build onClick calculate function for calButton
-$("").on("click", function (event) {
+// When user clicks calButton, 
+$(calButton).on("click", function (event) {
+
+});
+
+// Build onClick generate PDF function for 'a'
+// When user clicks 'a', a PDF is generated including 'a screenshot of the app in its current state'.
+// Then a link to a PDF is generated and then opened in the new window.
+$(pdfButton).on("click", function (event) {
 
 });
 
