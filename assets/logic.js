f20c87c7be367569c85ae51576c237a4abdc69c5
Greg Schuman <gcschuman@hotmail.com>
2020-02-06 20:19:12 -0800

    Latest and greatest updates

--- assets/logic.js
+++ assets/logic.js
@@ -14,144 +14,86 @@ let titleText = $(".titleText");
 let dateToday = $(".dateToday");
 let currentDate = moment().format('dddd') + " " + moment().format("MMM Do");
 dateToday.text(currentDate);
-let boxHeader = $(".boxHeader");
-        // let amountText = $(".ammountText");
-        // let currency1 = $(".currency1");
-        // let select = $(".select");
-let box1 = $("#box1");
-let currency1Ammount = $("#currency1Ammount").val();
-let updated1 = $(".updated1");
-        // let updatedTime1 = $("#updatedTime1");
-let blankArea = $(".blankArea");
-        // let currency2 = $(".currency2");
-let box2 = $("#box2");
-let currency2Ammount = $("#currency2Ammount").val();
-let updated2 = $("#updated2");
-        // let country2price = $("#country2price");
-let calButton = $("#calButton");
+
 let pdfButton = $(".pdfButton");
 
-// Storing our URL for a 'Live' currency request
-liveQueryURL = "http://www.api.currencylayer.com/live?access_key=4e8b520592221b3422775e55f28b2a2a&currencies=" + "";
+let boxHeader = $(".boxHeader");
+let amountText = $(".ammountText");
 
-// Storing our URL for a 'Historical' currency request
-// historicalQueryURL = "http://www.api.currencylayer.com/historical?access_key=4e8b520592221b3422775e55f28b2a2a;"
+let currency1 = $(".currency1");
+let select = $(".select");
+let box1 = $("#box1");
+let currency1Amount = $("#currency1Ammount");
+let updated1 = $(".updated1");
+let updatedTime1 = $("#updatedTime1");
 
-// Following request not supported by my api subscription
-            // // Storing our URL for a 'Convert' currency request
-            // convertQueryURL = "http://www.api.currencylayer.com/convert?access_key=4e8b520592221b3422775e55f28b2a2a&from=" + "" "&to=" + "";
+let blankArea = $(".blankArea");
+
+let currency2 = $(".currency2");
+let box2 = $("#box2");
+let currency2Amount = $("#currency2Ammount");
+let updated2 = $("#updated2");
+let country2price = $("#country2price");
+
+let calButton = $("#calButton");
 
 
 //-----BUILDING FUNCTIONS-----
-// Setting variable for all currencies that can be requested
-// This list will populate the dropdown menus
-function grabCurrencyList() {
-    console.log("Requesting Currency lists...");
-    let currencyListQueryURL = "http://api.currencylayer.com/list?access_key=4e8b520592221b3422775e55f28b2a2a"
-    $.ajax({
-        url: currencyListQueryURL,
-        method: "GET"
-    })
-    // After the data from the AJAX request comes back
-    .then(function(response) {
-        console.log(response);
-        // Saving the Currency List Data
-        currencyList = response.currencies;
-        currencyListArray = JSON.stringify(response.currencies);
-        console.log(currencyList);
-        console.log("# of Currencies supported on next line");
-        console.log(currencyListArray.length);
-    
-        // API pull for live currencies to be added to dropdown
-        console.log("Popluating Dropdown lists...");
-        // For each currency in the currency list array,
-        
-        $.each(currencyList, function(i, currency) {
-            box1.append("<option>" + currency + "</option>");
-        });
-
-        $.each(currencyList, function(i, currency) {
-            box2.append("<option>" + currency + "</option>");
-        });
-        
-        // Commenting put below for loop for now
-                // for (i = 0; i <= currencyListArray.length; i++) {
-                //     // Creating variable for options in dropDown menus
-                //     let currencyOptions = $("<option>");
-                //     $(currencyOptions).addClass("currencies");
-                //         // $(currencyOptions).attr("id", currencyListArray.i)
-                    
-                //     // Displaying currencies in the options
-                //     //  Currently trying to figure out how to target the data for each currency
-                //     currentCurrency = currencyListArray;
-                //     console.log(currentCurrency);
-                //     $(currencyOptions).text(currentCurrency);
-
-                //     // Append currencies to dropDown menus
-                //     // Currently, box 1 is blank and 2 is populated, but if i comment out box 2, box 1 works
-                //     $(box1).append(currencyOptions);
-                //     $(box2).append(currencyOptions);
-
-                // };
-    });
-};
-    
-
-
-// --Commenting out below function for now--
-        // button.addEventListener("click", function(name){
-        //     fetch("http://data.fixer.io/api/latest?access_key=046374ead830a8a183f4c823ed1d0bc2")
-        // for(i = 0; i<currency1.currency2; i++);
-        // });
+// API pull for the currencies to be added to dropdown
+button.addEventListener("click", function(name){
+    fetch("http://data.fixer.io/api/latest?access_key=046374ead830a8a183f4c823ed1d0bc2")
+for(i = 0; i<currency1.currency2; i++);
+});
 
 
 
 // Build onClick calculate function for calButton
 // When user clicks calButton, 
+$(calButton).on("click", function (event) {
 
-// Commented out until prerequisite functionality is in
-// $(calButton).on("click", function (event) {
-
-//     // Perfoming an AJAX GET request to our 'Live' queryURL
-//     $.ajax({
-//         url: liveQueryURL,
-//         method: "GET"
-//     })
-//     // After the data from the AJAX request comes back
-//     .then(function(response) {
-//         // Saving the Live Currency Data
-//         var liveResponse = response.data;
-//         // Displaying the currency2 value
-//         currency2Ammount.text("");
-//     });
-
-// });
+});
 
 
 
 // Build onClick generate PDF function for 'a'
 // When user clicks 'a', a PDF is generated including 'a screenshot of the app in its current state'.
 // Then a link to a PDF is generated and then opened in the new window.
+// $(pdfButton).on("click", function (event) {
+//     if (typeof doc.print === 'undefined') {    
+//         setTimeout(function(){printDocument(documentId);}, 1000);
+//     } else {
+//         doc.print();
+//     }
+// <embed
+//     type="application/pdf"
+//     src="path_to_pdf_document.pdf"
+//     id="pdfDocument"
+//     width="100%"
+//     height="100%" />
 
-// --Commenting out below function for now--
-        // $(pdfButton).on("click", function (event) {
-        //     if (typeof doc.print === 'undefined') {    
-        //         setTimeout(function(){printDocument(documentId);}, 1000);
-        //     } else {
-        //         doc.print();
-        //     }
-        // <embed
-        //     type="application/pdf"
-        //     src="path_to_pdf_document.pdf"
-        //     id="pdfDocument"
-        //     width="100%"
-        //     height="100%" />
-        // });
+    /* <embed
+        type="application/pdf"
+        src="path_to_pdf_document.pdf"
+        id="pdfDocument"
+        width="100%"
+        height="100%" />
+     */
+
+//     $(pdfButton).on("click", function (event) {
+//         function printDocument(documentId) {
+//             var doc = document.getElementById(documentId);
+        
+//             //Wait until PDF is ready to print    
+//             if (typeof doc.print === 'undefined') {    
+//                 setTimeout(function(){printDocument(documentId);}, 1000);
+//             } else {
+//                 doc.print();
+//             }
+//         }
+// });
 
 
 //-----CALLING FUNCTIONS-----
-// This function will run when the HTML has finished loading
-grabCurrencyList();
 
 
 });
