d80f2b7de19fa7a51a408cc38f59c3ef98c39be0
Matthew Bell <matthewbell1030@gmail.com>
2020-02-11 11:36:39 -0800

    for index.html, some class and id changes.  For logic.js, calculator button function and local storage are nearly complete, awaiting completion of localstorage.getitem section

--- assets/logic.js
+++ assets/logic.js
@@ -10,18 +10,11 @@ console.log("logic.js is successfully linked");
 //-----DECLARING GLOBAL VARIABLES-----
 let dateToday = $(".dateToday");
 let currentDate = moment().format('dddd') + " " + moment().format("MMM Do");
-    // let boxHeader = $(".boxHeader");
 let box1 = $("#box1");
-let updated1 = $(".updated1");
-let updated2 = $("#updated2");
-    // let blankArea = $(".blankArea");
 let box2 = $("#box2");
 let calButton = $("#calBtn");
     // let pdfButton = $(".pdfButton");
-
-let searchHistory = [];    
-let searchHistoryCurrency = [];
-let searchHistoryValue1 = [];
+let searchNum = 0;
 
 //-----BUILDING FUNCTIONS-----
 // Display Current Date at top of app
@@ -49,16 +42,16 @@ function grabCurrencyList() {
         // Grabbing currency keys
         currencyKeys = Object.keys(currencyList);
         console.log("currency keys", currencyKeys);
-
+        // Grabbing currency values
         currencyValues = Object.values(currencyList);
         console.log("currency values", currencyValues);
-
+        // Combining currency keys and values
         let currencyOptions = _.zip(currencyValues, currencyKeys);
         console.log("currencyOptions", currencyOptions);
 
         // API pull for live currencies to be added to dropdown
         console.log("Popluating Dropdown lists...");
-        // For each currency in the currency list array,
+        // For each currency in the currency list array, create an option containing 'currencyOptions'
         $.each(currencyOptions, function(i, currency) {
             box1.append("<option>" + currency + "</option>");
             box2.append("<option>" + currency + "</option>");
@@ -79,20 +72,25 @@ $(calButton).on("click", function (event) {
     // Creating variables for reading from and writing to currency ammount fields
     let currency1Ammount = $("#currency1Ammount").val();
     console.log("User currency 1 Value : ", currency1Ammount);
-    let currency2Ammount = $("#currency2Ammount").val();
+    let currency2Ammount = $("#currency2Ammount");
+
+    // Creating variables for meta data
+    let updated1 = $(".updated1");
+    let updated2 = $("#updated2");
 
     let box1Currency = $(box1).val();
     console.log("User currency 1 : ", box1Currency);
+    // 'box1CurrencyKey' is currently unused other than console.log for meta
     let box1CurrencyKey = box1Currency.substring(box1Currency.length - 3, box1Currency.length);
     console.log("User currency 1 KEY : ", box1CurrencyKey);
     let box2Currency = $(box2).val();
     console.log("User currency 2 : ", box2Currency);
+    // 'box2CurrencyKey' is currently unused other than console.log for meta
     let box2CurrencyKey = box2Currency.substring(box2Currency.length - 3, box2Currency.length);
     console.log("User currency 2 KEY : ", box2CurrencyKey);
 
     // Storing our URL for a 'Live' currency request
     liveQueryURL = "http://api.currencylayer.com/live?access_key=4e8b520592221b3422775e55f28b2a2a&currencies=" + box1Currency + box2Currency + "&format=1";
-
     // Perfoming an AJAX GET request to our 'Live' queryURL
     $.ajax({
         url: liveQueryURL,
@@ -128,17 +126,63 @@ $(calButton).on("click", function (event) {
         // Multiply quoteValue by user provided currency1Ammount
         convertedAmmount = currency1Ammount * quoteValue;
         // Display convertedAmmount in currency2Ammount
-        $(currency2Ammount).html(convertedAmmount);
+        $(currency2Ammount).val(convertedAmmount);
         console.log("Returned currency 2 Value : ", convertedAmmount);
 
-        // Beginning with quoteValue, move decimal point over two places to the left to find the exchangeRate
-        exchangeRate = quoteValue * .01;
+        // Beginning with quoteValue, move decimal point over two places to the right to find the exchangeRate percentge
+        exchangeRate = (quoteValue * 100) + " %";
         console.log("Exchange Rate : ", exchangeRate);
         // Display exchangeRate in field below currency2Ammount
         $(updated2).text(exchangeRate);
 
-        // searchHistoryCurrency.push();
+        // Creating Search History nested arrays to push to and pull from
+        // Push all search information into arrays, and nest those arrays into an object       
 
+        // OPTION 1 for history; both work, just different output
+        // let searchHistoryCurrency1 = [];
+        // searchHistoryCurrency1.push(box1Currency);
+        // let searchHistoryCurrency2 = [];
+        // searchHistoryCurrency2.push(box2Currency);
+        // let searchHistoryCurrencies = _.zip(searchHistoryCurrency1, searchHistoryCurrency2); 
+
+        // let searchHistoryValue1 = [];
+        // searchHistoryValue1.push(currency1Ammount);
+        // let searchHistoryValue2 = [];
+        // searchHistoryValue2.push(convertedAmmount);
+        // let searchHistoryValues = _.zip(searchHistoryValue1, searchHistoryValue2);
+        
+        // let searchHistoryDate = [];
+        // searchHistoryDate.push(lastUpdated);
+        // let searchHistoryRate = [];
+        // searchHistoryRate.push(exchangeRate);
+        // let searchHistoryMeta = _.zip(searchHistoryDate, searchHistoryRate);
+
+        // let searchHistory = _.zip(searchHistoryCurrencies, searchHistoryValues, searchHistoryMeta);
+
+        // OPTION 2 for history; both work, just different output
+        let searchHistory = {
+            box1Currency,
+            box2Currency,
+            currency1Ammount,
+            convertedAmmount,
+            lastUpdated,
+            exchangeRate
+        }
+        console.log(searchHistory);
+
+        // Pushing searches to local storage
+        searchNum ++ ;
+        let searchHistoryArray = [];
+        searchHistoryArray.push(searchHistory);
+        localStorage.setItem("search" + [searchNum],JSON.stringify(searchHistoryArray));
+    
+        // Pulling searches from local storage
+        // localStorage.getItem("search" + [searchNum],);
+
+
+
+        console.log("Ending 'Calculate Button' on-Click function");
+        console.log("-----------------------------------");
     });
 
 // End of calButton click function
